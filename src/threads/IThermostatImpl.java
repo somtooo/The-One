da163b306bfd77bdda6d591d2da2aaf05d4bc696
csomtoo <chukwurahb@gmail.com>
2022-02-01 23:06:12 -0500

    Finished Thermostat IMPL

---
+++ src/threads/IThermostatImpl.java
@@ -0,0 +1,38 @@
+package threads;
+
+public class IThermostatImpl implements IThermostat {
+  @Override
+  public void setMode(Mode mode) {
+
+  }
+
+  @Override
+  public void setHeat(double degrees) {
+
+  }
+
+  @Override
+  public void setCool(double degrees) {
+
+  }
+
+  @Override
+  public void targetTemperature(double degrees) {
+
+  }
+
+  @Override
+  public void turnFan(int duration) {
+
+  }
+
+  @Override
+  public boolean isConnected() {
+    return false;
+  }
+
+  @Override
+  public void subscribe(DeviceClient client) {
+
+  }
+}
